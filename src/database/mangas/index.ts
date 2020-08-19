d3918e8e565f60aeae9992b1bb7a9e13d9a2603e
Renaud LEGAN <rlegan@student.42.fr>
2020-08-19 11:56:58 +0200

    Added database integration

---
+++ src/database/mangas/index.ts
@@ -0,0 +1,15 @@
+import { MangaModel } from "./MangaModel";
+import MangaSchema from "./MangaSchema";
+import {
+  MangaInterface,
+  MangaInterfaceDocument,
+  MangaInterfaceModel,
+} from "./MangaTypes";
+
+export {
+  MangaModel,
+  MangaInterface,
+  MangaInterfaceDocument,
+  MangaInterfaceModel,
+  MangaSchema,
+};
